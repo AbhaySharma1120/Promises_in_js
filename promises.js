b7aa36b022ac8d0f854e8e6643cd6f27fbd80538
AbhaySharma1120 <abhaysharma4762@gmail.com>
2026-03-16 19:21:37 +0530

    promises using async await

--- promises.js
+++ promises.js
@@ -50,4 +50,51 @@ promiseFour.then((user) => {
     console.log(username);
 }).catch(function(error){
     console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"));
+
+//Promise 5
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
+
+
+// fetch
+// async function getAllUsers(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch(error){
+//         console.log("E: ", error);
+//     }
+// }
+
+// getAllUsers();
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json();
 })
+.then((data) => {
+    console.log(data)
+})
+.catch((error) => console.log(error))
